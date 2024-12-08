916c0e3c208eaca18499df9f1aa189843d88763e
Taketo Yahiro (Yapimaru) <yahiro.t.eng@gmail.com>
2024-12-08 14:23:50 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const dist = path.resolve(__dirname, "dist");
+
+module.exports = {
+  mode: "production",
+  experiments: {
+    asyncWebAssembly: true,
+  },
+  entry: {
+    index: "./pkg/index.js",
+  },
+  output: {
+    path: dist,
+    filename: "[name].js",
+  },
+  devServer: {
+    static: dist,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./static/index.html"
+    }),
+    new WasmPackPlugin({
+      crateDirectory: __dirname,
+    }),
+  ]
+};
